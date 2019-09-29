c2b5380231e19f69ecbf5cefbbacc0855f12dfce
Gurleen Sethi <sarusethi@rocketmail.com>
2019-09-28 20:50:14 -0400

    Add basic tic tac toe version.

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,32 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Game(),
+      title: 'Tic Tact Toe',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tic Tac Toe'),
+      ),
+      body: Center(
+        child: Game(),
+      ),
     );
   }
 }
 
 class Square extends StatelessWidget {
-  final int index;
   final String text;
-  final void Function(int index) onTap;
+  final void Function() onTap;
 
   const Square({
     Key key,
-    this.index,
     this.text,
     this.onTap,
   }) : super(key: key);
@@ -28,17 +41,15 @@ class Square extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => this.onTap(this.index),
+      onTap: this.onTap,
       child: Container(
         width: 60.0,
         height: 60.0,
         decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.black,
-          ),
+          border: Border.all(),
         ),
         child: Center(
-          child: Text(this.text ?? ''),
+          child: Text(this.text ?? ""),
         ),
       ),
     );
@@ -46,12 +57,12 @@ class Square extends StatelessWidget {
 }
 
 class Board extends StatelessWidget {
-  final List<String> squares;
+  final List<String> data;
   final void Function(int index) onTap;
 
   const Board({
     Key key,
-    this.squares,
+    this.data,
     this.onTap,
   }) : super(key: key);
 
@@ -60,16 +71,15 @@ class Board extends StatelessWidget {
     return Container(
       width: 200.0,
       child: GridView.builder(
+        itemCount: 9,
         shrinkWrap: true,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 3,
         ),
-        itemCount: 9,
         itemBuilder: (context, index) {
           return Square(
-            index: index,
-            onTap: this.onTap,
-            text: squares[index],
+            onTap: () => this.onTap(index),
+            text: this.data[index],
           );
         },
       ),
@@ -83,99 +93,63 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
-  final List<List<String>> _history = [
-    List.generate(9, (index) => null),
-  ];
-  int _currentStep = 0;
-  bool _isXNext = true;
+  List<String> _gameData = List.generate(9, (index) => null);
+  bool _isXTurn = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tic Tac Toe'),
-      ),
-      body: Column(
+    return Container(
+      child: Column(
         children: <Widget>[
-          Text(_getStatus()),
           Board(
-            squares: _history[_currentStep],
-            onTap: _handleClick,
+            data: _gameData,
+            onTap: _handleTap,
           ),
-          Text('Time Travel'),
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: _history.length,
-              itemBuilder: (context, index) {
-                final text = index == 0 ? 'Game Start' : 'Move to step $index';
-
-                return RaisedButton(
-                  child: Text(text),
-                  onPressed: () {
-                    this.setState(() {
-                      _currentStep = index;
-                    });
-                  },
-                );
-              },
-            ),
-          ),
+          Text(_getTurn()),
         ],
       ),
     );
   }
 
-  void _handleClick(index) {
-    final squares = _history[_currentStep];
-    final winner = _calculateWinner(squares);
-    if (winner != null || squares[index] != null) {
-      return;
+  String _getTurn() {
+    final winner = _calculateWinner(this._gameData);
+    if (winner != null) {
+      return "Winner is $winner";
     }
+    return "Current turn is ${_isXTurn ? "X" : "O"}";
+  }
 
-    final move = this._isXNext ? "X" : "O";
-    final newSquares = squares.sublist(0);
-    newSquares[index] = move;
-
+  void _handleTap(int index) {
+    final winner = _calculateWinner(this._gameData);
+    if (_gameData[index] != null || winner != null) {
+      return null;
+    }
+    _gameData[index] = _isXTurn ? "X" : "O";
     setState(() {
-      if (_history.length > (_currentStep + 1)) {
-        _history.removeRange(_currentStep + 1, _history.length);
-      }
-      _history.add(newSquares);
-      _currentStep = _history.length - 1;
-      _isXNext = !_isXNext;
+      _isXTurn = !_isXTurn;
     });
   }
 
-  String _getStatus() {
-    final winner = _calculateWinner(_history[_currentStep]);
-    if (winner != null) {
-      return 'The winner is $winner';
-    } else {
-      return 'Next turn is ${this._isXNext ? "X" : "O"}';
+  String _calculateWinner(List<String> squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (int i = 0; i < lines.length; i++) {
+      List<int> line = lines[i];
+      if (squares[line[0]] == squares[line[1]] &&
+          squares[line[1]] == squares[line[2]]) {
+        return squares[line[0]];
+      }
     }
+
+    return null;
   }
-}
-
-String _calculateWinner(List<String> squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (int i = 0; i < lines.length; i++) {
-    List<int> line = lines[i];
-    if (squares[line[0]] == squares[line[1]] &&
-        squares[line[1]] == squares[line[2]]) {
-      return squares[line[0]];
-    }
-  }
-
-  return null;
 }
